77af1ce4e9a8009b4e6c7b0c78fd2a039f8d46ef
favian.ramos <favian.ramos@SBBOGLAPPROFR7.sophos.col.com>
2020-03-10 17:42:51 -0500

    Recibiendo parametros de otra forma por http request

--- spring-boot-web/src/main/java/com/favian/springboot/web/app/controllers/EjemploParamsController.java
+++ spring-boot-web/src/main/java/com/favian/springboot/web/app/controllers/EjemploParamsController.java
@@ -1,10 +1,13 @@
 package com.favian.springboot.web.app.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.thymeleaf.standard.expression.NumberTokenExpression;
 
 @Controller
 @RequestMapping("/params")
@@ -22,5 +25,25 @@ public class EjemploParamsController {
 		
 	}
 	
+	@GetMapping("/mix-params") 
+	public String param(@RequestParam String saludo,@RequestParam Integer numero,  Model model) {
+		model.addAttribute("resultado", "El saludo enviado es: '" + saludo + "' ye l numero es :" + numero +"'");
+	return "params/ver";
+		
+	}
+	
+	@GetMapping("/mix-params-request") 
+	public String param(HttpServletRequest request,  Model model) {
+		String saludo =request.getParameter("saludo");
+		Integer numero=null;
+		try {
+		numero= Integer.parseInt(request.getParameter("numero"));
+		} catch(NumberFormatException e) {
+		numero=0;	 
+		}
+		model.addAttribute("resultado", "El saludo enviado es: '" + saludo + "' ye l numero es :" + numero +"'");
+	return "params/ver";
+		
+	}
 	
 }
