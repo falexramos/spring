29d98e008eefc3474e89691c9c73cc402905789b
favian.ramos <favian.ramos@SBBOGLAPPROFR7.sophos.col.com>
2020-03-10 17:23:14 -0500

    1 Creando. Creando listas y pasandolas a la vista 2 Creando y pasando parametros por metodo get.

--- spring-boot-web/src/main/java/com/favian/springboot/web/app/controllers/IndexController.java
+++ spring-boot-web/src/main/java/com/favian/springboot/web/app/controllers/IndexController.java
@@ -1,11 +1,17 @@
 package com.favian.springboot.web.app.controllers;
 
-import java.util.Map;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.favian.springboot.web.app.models.Usuario;
 
 @Controller
 @RequestMapping("/app")
@@ -17,5 +23,35 @@ public class IndexController {
 		return "index";
 	}
 	
-	
+@RequestMapping("/perfil")
+public String perfil(Model model) {
+	Usuario usuario = new Usuario();
+	usuario.setNombre("Andrés");
+	usuario.setApellido("Ramos");
+	usuario.setEmail("correo@gmail.com");
+	model.addAttribute("usuario", usuario);
+	model.addAttribute("titulo","Perfil de usuario: ".concat(usuario.getNombre()));
+	return "perfil";
+	}
+
+
+@RequestMapping("/listar")
+public String listar(Model model) {
+		 
+		model.addAttribute("titulo", "Listado de Uusarios");
+		
+	return "listar";
+	}
+
+@ModelAttribute("usuarios")
+public List<Usuario> poblarUsuarios(){
+	List<Usuario> usuarios = Arrays.asList(
+			new Usuario("Andrés","Ramos","correo@gmail.com"),
+			new Usuario("Jhon","Doe","jhon@gmail.com"),
+			new Usuario("Jane","Doe","Jane@gmail.com"),
+			new Usuario("tornado","Roe","roe@gmail.com")
+			);
+	return usuarios;
+	}
+
 }
