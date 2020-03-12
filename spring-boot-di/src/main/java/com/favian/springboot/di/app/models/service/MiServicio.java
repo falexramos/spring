82a1e6af510b18f76b8c060b45baf97cbc8fb4e4
favian.ramos <favian.ramos@SBBOGLAPPROFR7.sophos.col.com>
2020-03-12 08:41:37 -0500

    inyectando por autowired

--- spring-boot-di/src/main/java/com/favian/springboot/di/app/models/service/MiServicio.java
+++ spring-boot-di/src/main/java/com/favian/springboot/di/app/models/service/MiServicio.java
@@ -1,9 +1,12 @@
 package com.favian.springboot.di.app.models.service;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class MiServicio {
 
 	public String operacion() {
 		
-		return "Ejecutando un proceso importante...";
+		return "Ejecutando un proceso importante";
 	}
 }
