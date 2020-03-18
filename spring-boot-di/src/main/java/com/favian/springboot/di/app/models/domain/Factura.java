e7e090707a6f7f7059ee4169c8be25bbd00c509f
favian.ramos <favian.ramos@SBBOGLAPPROFR7.sophos.col.com>
2020-03-18 16:05:35 -0500

    1 contextos en spring 2 @SessionScope es para trabajar con sessiones

--- spring-boot-di/src/main/java/com/favian/springboot/di/app/models/domain/Factura.java
+++ spring-boot-di/src/main/java/com/favian/springboot/di/app/models/domain/Factura.java
@@ -9,8 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.SessionScope;
 
 @Component
+@SessionScope
 public class Factura {
 	
 	
